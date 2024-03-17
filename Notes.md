06abf5620251541fe8834652b9c0881bcb971608
erick-sudo <erickocchieng766@gmail.com>
2024-03-17 18:22:25 +0300

    Views

--- Notes.md
+++ Notes.md
@@ -1,11 +1,25 @@
-# Laravel Notes
+# Laravel
 
 ## Commands
 
 ```sh
 
+    # Setup
+
     sudo apt-get install php-xml
     sudo apt-get install php-curl
     sudo apt-get install php-sqlite3
-    
+
+    # Make Migration
+    php artisan make:migration create_tasks_table --create=tasks
+
+    # Migrate
+    php artisan migrate
+	
+    # Create Eloquent models
+    php artisan make:model Task
+
+	
 ```
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,48 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
+
+/**
+ * Display All Tasks
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add a New Task
+ */
+Route::post('/tasks', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255'
+    ]);
+
+    if($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create task
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete a Task
+ */
+Route::delete('/tasks/{id}', function($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
 });
